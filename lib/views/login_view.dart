05babf08003f8ea6f2c8e374258c59de5604b2ad
Joshua Chihozhwa <32734861+Joshsnailz@users.noreply.github.com>
2024-04-22 23:19:34 +0100

    fixed the log in

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -62,19 +62,22 @@ class _LoginViewState extends State<LoginView> {
               final email = _email.text;
               final password = _password.text;
               try {
-                final userCredential = await FirebaseAuth.instance
-                    .signInWithEmailAndPassword(
-                        email: email, password: password);
-                
-
-                Navigator.of(context).pushNamedAndRemoveUntil(
-                  userblogsRoute,
-                  (route) => false,
-                );
+                await FirebaseAuth.instance.signInWithEmailAndPassword(
+                    email: email, password: password);
+                final user = FirebaseAuth.instance.currentUser;
+                if (user?.emailVerified ?? false) {
+                  Navigator.of(context).pushNamedAndRemoveUntil(
+                    userblogsRoute,
+                    (route) => false,
+                  );
+                } else {
+                  Navigator.of(context).pushNamedAndRemoveUntil(
+                      verifyEmailRoute, (route) => false);
+                }
               } on FirebaseAuthException catch (e) {
                 await showErrorDialog(context, e.message.toString());
                 devtools.log(e.code);
-              }catch (e){
+              } catch (e) {
                 await showErrorDialog(context, e.toString());
                 devtools.log(e.toString());
               }
